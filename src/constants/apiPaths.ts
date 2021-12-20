81dc06bc23d045b048ebd5851e0df5a1c3f4083b
Alexander Teplov <a.v.teplov@gmail.com>
2021-12-20 06:02:28 +0300

    task-3: switched /products to API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  products: 'https://k6cbtanpjd.execute-api.eu-west-1.amazonaws.com/dev/products',
+  order: 'https://k6cbtanpjd.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://k6cbtanpjd.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://k6cbtanpjd.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://k6cbtanpjd.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
